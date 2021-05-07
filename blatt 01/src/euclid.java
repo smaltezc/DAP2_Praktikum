c2d32df366daaa1e27394e84de98060cd9eb053f
smaltezc <72527003+smaltezc@users.noreply.github.com>
2021-05-08 00:42:19 +0200

    Add files via upload

--- blatt 01/src/euclid.java
+++ blatt 01/src/euclid.java
@@ -1,34 +1,23 @@
-import java.util.Scanner;
 
 public class euclid {
 
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        String[] partsOfInput = input.split(" ");
-        int laenge = partsOfInput.length;
-        if(laenge == 4)
+        if(args.length !=2 )
         {
-            String first = partsOfInput[0];
-            String second = partsOfInput[1];
-            String third = partsOfInput[2];
-            String fourth = partsOfInput[3];
-            if(first.equals("java") && second.equals( "Euclid") && isNumeric(third) && isNumeric(fourth) )
+            System.out.println("Falsche Parameteranzahl!");
+            System.out.println("Aufruf mit: java Euclid a b");
+            System.out.println("Dabei muessen a und b natuerliche Zahlen groesser 0 sein.");
+            System.out.println("Beispiel: java Euclid 24 896");
+            return;
+        }
+
+        try {
+            int a = Integer.parseInt(args[0]);
+            int b = Integer.parseInt(args[1]);
+            if (a > 0 && b > 0)
             {
-                int a = Integer.parseInt(partsOfInput[2]);
-                int b = Integer.parseInt(partsOfInput[3]);
-                if (a > 0 && b > 0)
-                {
-                    System.out.println(fnct(a,b));
-                }
-                else
-                {
-                    System.out.println("Falscher Parameter - Nur Zahlen sind erlaubt!");
-                    System.out.println("Aufruf mit: java Euclid a b");
-                    System.out.println("Dabei muessen a und b natuerliche Zahlen groesser 0 sein.");
-                    System.out.println("Beispiel: java Euclid 24 896");
-                }
+                System.out.println(fnct(a,b));
             }
             else
             {
@@ -38,20 +27,15 @@ public class euclid {
                 System.out.println("Beispiel: java Euclid 24 896");
             }
         }
-        else
+        catch (Exception e)
         {
-            System.out.println("Falsche Parameteranzahl!");
+            System.out.println("Falscher Parameter - Nur Zahlen sind erlaubt!");
             System.out.println("Aufruf mit: java Euclid a b");
             System.out.println("Dabei muessen a und b natuerliche Zahlen groesser 0 sein.");
             System.out.println("Beispiel: java Euclid 24 896");
         }
 
     }
-
-    /*
-    int a = Integer.parseInt(partsOfInput[0]);
-    int b = Integer.parseInt(partsOfInput[1]);
-     */
 
     public static int fnct(int a, int b)
     {
@@ -62,15 +46,6 @@ public class euclid {
         else
         {
             return fnct(b, (a % b) );
-        }
-    }
-
-    public static boolean isNumeric(String str) {
-        try {
-            Double.parseDouble(str);
-            return true;
-        } catch(NumberFormatException e){
-            return false;
         }
     }
 }
--- blatt 01/src/primeNums.java
+++ blatt 01/src/primeNums.java
@@ -1,156 +1,115 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class primeNums {
 
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        String[] partsOfInput = input.split(" ");
-        int laenge = partsOfInput.length;
-        if(laenge == 3)
+
+        if(args.length == 1)
         {
-            if(hasNums(input))
-            {
-                if(partsOfInput[0].equals("java") &&
-                        partsOfInput[1].equals("Eratosthenes") )
+            try {
+                int n = Integer.parseInt(args[0]);
+                if (n > 2)
                 {
-                    int n = Integer.parseInt(partsOfInput[2]);
-                    if(n>2)
+                    boolean[] isPrime = new boolean[n+1];
+                    Arrays.fill(isPrime, true);
+                    int count = 0;
+                    for(int i = 0; i< isPrime.length ; i++)
                     {
-
-                        boolean[] isPrime = new boolean[n];
-                        Arrays.fill(isPrime, true);
-                        int count = 0;
-                        for(int i = 0; i< isPrime.length ; i++)
+                        for(int j = 2; j < i ; j++)
                         {
-                            for(int j = 2; j < i ; j++)
+                            if(i % j == 0)
                             {
-                                if(i % j == 0)
-                                {
-                                    isPrime[i]=false;
-                                    count++;
-                                    break;
-                                }
+                                isPrime[i]=false;
+                                count++;
+                                break;
                             }
-
                         }
-                        //Hier ist -2 für i = 0 1
-                        int anzahlPrimes = n-count-2;
-                        System.out.println(anzahlPrimes);
 
                     }
-                    else if (n>0)
-                    {
-                        System.out.println(0);
-                    }
-                    else
-                    {
-                        System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
-                        System.out.println("Aufruf mit : java Eratosthenes n [-o]");
-                        System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
-                        System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
-                        System.out.println("Bsp: java Eratosthenes 100 -o");
-
-                    }
+                    //wegen n+1 müssen wir um -1 reduzieren
+                    int anzahlPrimes = n-count-1;
+                    System.out.println(anzahlPrimes);
                 }
                 else
                 {
-                    System.out.println("Falscher Parameter!");
+                    System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
                     System.out.println("Aufruf mit : java Eratosthenes n [-o]");
                     System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
                     System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
                     System.out.println("Bsp: java Eratosthenes 100 -o");
                 }
             }
-            else
+            catch (Exception e)
             {
-                System.out.println("Falscher Parameter!");
+                System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
                 System.out.println("Aufruf mit : java Eratosthenes n [-o]");
                 System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
                 System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
                 System.out.println("Bsp: java Eratosthenes 100 -o");
             }
-
         }
-        else if(laenge == 4)
+        else if(args.length == 2)
         {
-            if(hasNums(input))
-            {
-                if(partsOfInput[0].equals("java") &&
-                        partsOfInput[1].equals("Eratosthenes") &&
-                        partsOfInput[3].equals("-o") )
+            try {
+                int n = Integer.parseInt(args[0]);
+                if (n > 2)
                 {
-                    int n = Integer.parseInt(partsOfInput[2]);
-                    if(n>2)
+                    boolean[] isPrime = new boolean[n+1];
+                    Arrays.fill(isPrime, true);
+                    int countNotPrimes = 0;
+                    for(int i = 0; i< isPrime.length ; i++)
                     {
-
-                        boolean[] isPrime = new boolean[n];
-                        Arrays.fill(isPrime, true);
-                        int count = 0;
-                        for(int i = 0; i< isPrime.length ; i++)
+                        for(int j = 2; j < i ; j++)
                         {
-                            for(int j = 2; j < i ; j++)
+                            if(i % j == 0)
                             {
-                                if(i % j == 0)
-                                {
-                                    isPrime[i]=false;
-                                    count++;
-                                    break;
-                                }
-                            }
-
-                        }
-                        //Hier ist -2 für i = 0 1
-                        int anzahlPrimes = n-count-2;
-                        int arrayPrimes[] = new int[anzahlPrimes];
-                        int m=0;
-                        for(int x = 2; x< isPrime.length; x++)
-                        {
-                            if(isPrime[x])
-                            {
-                                arrayPrimes[m]=x;
-                                m++;
+                                isPrime[i]=false;
+                                countNotPrimes++;
+                                break;
                             }
                         }
-                        System.out.println(anzahlPrimes);
+
+                    }
+                    //wegen n+1 müssen wir um -1 reduzieren
+                    int anzahlPrimes = n-countNotPrimes-1;
+                    int arrayPrimes[] = new int[anzahlPrimes];
+
+                    int m=0;
+                    for(int x = 2; x< isPrime.length; x++)
+                    {
+                        if(isPrime[x])
+                        {
+                            arrayPrimes[m]=x;
+                            m++;
+                        }
+                    }
+                    System.out.println(anzahlPrimes);
+                    if(args[1].equals("-o"))
+                    {
                         for (int element: arrayPrimes) {
                             System.out.print(element+ " ");
                         }
-                    }
-                    else if (n>0)
-                    {
-                        System.out.println(0);
-                    }
-                    else
-                    {
-                        System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
-                        System.out.println("Aufruf mit : java Eratosthenes n [-o]");
-                        System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
-                        System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
-                        System.out.println("Bsp: java Eratosthenes 100 -o");
-
+                        System.out.println();
                     }
                 }
                 else
                 {
-                    System.out.println("Falscher Parameter!");
+                    System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
                     System.out.println("Aufruf mit : java Eratosthenes n [-o]");
                     System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
                     System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
                     System.out.println("Bsp: java Eratosthenes 100 -o");
                 }
             }
-            else
+            catch (Exception e)
             {
-                System.out.println("Falscher Parameter!");
+                System.out.println("Falscher Parameter! Nur Integer groesser 0 sind erlaubt.");
                 System.out.println("Aufruf mit : java Eratosthenes n [-o]");
                 System.out.println("Es wird die Anzahl der Primzahlen aus dem Bereich [2,n] berechnet.");
                 System.out.println("Mit -o werden diese Zahlen auch ausgegeben.");
                 System.out.println("Bsp: java Eratosthenes 100 -o");
             }
-
         }
         else
         {
@@ -161,10 +120,5 @@ public class primeNums {
             System.out.println("Bsp: java Eratosthenes 100 -o");
         }
 
-    }
-
-    public static boolean hasNums(String text) {
-
-        return text.matches(".*\\d.*");
     }
 }
